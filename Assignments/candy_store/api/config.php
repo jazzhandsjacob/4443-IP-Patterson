6afed4ae68792f75074010901ca71e140e0ea096
jazzhandsjacob <35586549+jazzhandsjacob@users.noreply.github.com>
2019-08-09 22:33:22 -0500

    Add files via upload

---
+++ Assignments/candy_store/api/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$auth = [
+    'user' => 'web_user',
+    'password' => '7w51mYGpuX9sPnZ0',
+    'host' => 'localhost',
+    'db' => 'web_user'
+];
+
+$secret_key = "MaC8hv9mvKXWFR+cVuZiUcZ4oJqDhs6LYrTuty94cVA=";
---
+++ Assignments/waldo/api/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$auth = [
+    'user' => 'web_user',
+    'password' => '7w51mYGpuX9sPnZ0',
+    'host' => 'localhost',
+    'db' => 'web_user'
+];
+
+
